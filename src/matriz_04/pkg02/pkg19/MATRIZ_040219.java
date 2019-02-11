62c698f2021e9ae5499437a8b67534255dd4e357
NellyLlamileth <NellyLlamileth@Usopersonal>
2019-02-11 17:54:07 -0600

    Programa de Matrices y Vectores con ISR AHORA

--- src/matriz_04/pkg02/pkg19/MATRIZ_040219.java
+++ src/matriz_04/pkg02/pkg19/MATRIZ_040219.java
@@ -1,5 +1,6 @@
 package matriz_04.pkg02.pkg19;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
@@ -9,9 +10,10 @@ public class MATRIZ_040219 {
     public static void main(String[] args) {
         Scanner Leer =new Scanner(System.in);//Declaramos un Scanner para que lea los dato.
         String StrMatriz [][];//Declaramos una Matriz tipo String.
-        StrMatriz = new String[10][6];//Le damos tamaño a la matriz.
-        int intSuelBas=0, intTotDedu=0, intTotPer=0, intSuelLi1=0, intSuelLi2=0, intSuelLi3=0, intSuelLi4=0, intDep;//Declaramos unas variables tipo entero para los calculos con numeros.
-        String strSuelBas1, strTotDedu1, strTotPer1, strSuelLi5, strDep1;//Declaramos unas variables String para poder mostrar
+        StrMatriz = new String[10][7];//Le damos tamaño a la matriz.
+        int intSuelBas=0, intTotDedu=0, intTotPer=0, intSuelLi1=0, intSuelLi2=0, intSuelLi3=0, intSuelLi4=0, intDep=0 ;//Declaramos unas variables tipo entero para los calculos con numeros.
+        String strSuelBas1, strTotDedu1, strTotPer1, strSuelLi6, strDep1, strIRS2;//Declaramos unas variables String para poder mostrar
+        double  dblISR1=0, dblSuelLi4=0, dblSuelLi5=0;
         System.out.println("Bienvenido!!!! INGRESO DE PLANILLA\n");
         
         for(int intCont=0; intCont<10; intCont++){//Pedimos los Nombres de los empleados
@@ -20,7 +22,7 @@ public class MATRIZ_040219 {
         }
         
         for(int intCont=0; intCont<10; intCont++){//Se ingresa un numero aleatorio en la variable entera
-          intSuelBas=(int)(Math.random()*5000+4000);
+          intSuelBas=(int)(Math.random()*99000+1000);
           strSuelBas1= Integer.toString(intSuelBas);//Se vuelve String la varible entera           
           StrMatriz[intCont][1]=strSuelBas1;//Se Guarda en la Matriz
         }
@@ -37,26 +39,43 @@ public class MATRIZ_040219 {
           StrMatriz[intCont][3]=strTotPer1;//Se Guarda en la Matriz
         }
         
+        
         for(int intCont=0; intCont<10; intCont++){
-          intSuelLi1= Integer.parseInt(StrMatriz[intCont][1]);//Se toman los valores en dichas posiciones de la matriz para tomar su dato y convertirlo a entero
+        intSuelLi1= Integer.parseInt(StrMatriz[intCont][1]);
+        if(intSuelLi1<=5000){
+        dblISR1= intSuelLi1*0.3;
+        }
+        if(intSuelLi1>=5001&&intSuelLi1<=10000){
+        dblISR1= intSuelLi1*0.5;
+        }
+        if(intSuelLi1>=10001&&intSuelLi1<=100000){
+        dblISR1= intSuelLi1*0.10;
+        }
+        strIRS2= String.valueOf((String.format("%.2f", dblISR1)));
+        StrMatriz[intCont][4]=strIRS2;
+        }
+        
+        for(int intCont=0; intCont<10; intCont++){
+          intSuelLi1= Integer.parseInt(StrMatriz[intCont][1]);//Se toman los valores en dichas posiciones de la matriz para tomar su dato y convertirlo a enteros
           intSuelLi2= Integer.parseInt(StrMatriz[intCont][2]);
           intSuelLi3= Integer.parseInt(StrMatriz[intCont][3]);
-          intSuelLi4= intSuelLi1-intSuelLi2+intSuelLi3;//Con los datos tomados de las posiciones anteriores se usaran para este calculo y se guarda en una variable String
-          strSuelLi5= Integer.toString(intSuelLi4);//Se vuelve String la varible entera 
-          StrMatriz[intCont][4]=strSuelLi5;//Se Guarda en la Matriz 
+          dblSuelLi4= Double.parseDouble(StrMatriz[intCont][4]);//Se toman los valores en dichas posiciones de la matriz para tomar su dato y convertirlo a double
+          dblSuelLi5= intSuelLi1-intSuelLi2+intSuelLi3-dblSuelLi4;//Con los datos tomados de las posiciones anteriores se usaran para este calculo y se guarda en una variable String
+          strSuelLi6= String.valueOf((String.format("%.2f", dblSuelLi5)));//Se vuelve String la varible double
+          StrMatriz[intCont][5]=strSuelLi6;//Se Guarda en la Matriz 
         }
         
         for(int intCont=0; intCont<10; intCont++){//Se ingresa un numero aleatorio en la variable entera  
           intDep=(int)(Math.random()*5+1);
           strDep1= Integer.toString(intDep);//Se vuelve String la varible entera 
-          StrMatriz[intCont][5]=strDep1;//Se Guarda en la Matriz
+          StrMatriz[intCont][6]=strDep1;//Se Guarda en la Matriz
         }
         
-        System.out.println("        <Nombre>         <Sue_Ba>       <Tot_De>         <Tot_Pe>        <Sue_Li>      <Depart>");
+        System.out.println("              <Nombre>                 <Sue_Ba>                 <Tot_De>                 <Tot_Pe>                 <ISR>                 <Sue_Li>                       <Depart>");
         
         for(int intFila=0; intFila<10; intFila++){//Se muesta la Matriz
-           for(int intColumna=0; intColumna<6; intColumna++){
-           System.out.print("           "+StrMatriz[intFila][intColumna]+"\t");
+           for(int intColumna=0; intColumna<7; intColumna++){
+           System.out.print("                  "+StrMatriz[intFila][intColumna]+"\t");
            }
            System.out.println();
         }
@@ -65,19 +84,19 @@ public class MATRIZ_040219 {
         int intD1=0,intD2=0,intD3=0,intD4=0,intD5=0;//Se declaran varibales tipo entero como contadores para los departamentos
         for(int Cont=0; Cont<10; Cont++)//se Buscara en las posiciones de la matriz el #departamento para poder contabilizar cuantos empleados hay en cada departamento.
         {
-        if(StrMatriz[Cont][5].equals("1")){
+        if(StrMatriz[Cont][6].equals("1")){
         intVector[0]=intD1=intD1+1;
         }
-        if(StrMatriz[Cont][5].equals("2")){
+        if(StrMatriz[Cont][6].equals("2")){
         intVector[1]=intD2=intD2+1;
         }
-        if(StrMatriz[Cont][5].equals("3")){
+        if(StrMatriz[Cont][6].equals("3")){
         intVector[2]=intD3=intD3+1;
         }
-        if(StrMatriz[Cont][5].equals("4")){
+        if(StrMatriz[Cont][6].equals("4")){
         intVector[3]=intD4=intD4+1;
         }
-        if(StrMatriz[Cont][5].equals("5")){
+        if(StrMatriz[Cont][6].equals("5")){
         intVector[4]=intD5=intD5+1;
         }
          }
